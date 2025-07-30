63187401e56197a9f7667ab1ee90664972b6a56c
omina08 <faxriddinovaomina8@email.com>
2025-07-30 21:40:14 +0500

    Navbar yangilandi

--- src/companents/Navbar.jsx
+++ src/companents/Navbar.jsx
@@ -5,20 +5,20 @@ const Navbar = () => {
   return (
     <div>
       <div className="flex items-center justify-between bg-gray-100 p-4 rounded-xl shadow-sm mt-[10px]">
-        {/* Logo */}
+ 
         <div className="flex items-center space-x-2">
           <img className="w-12 h-10 bg-gray-300 rounded-lg" src="/logo.jpg" alt="logo" />
           <span className="text-lg font-semibold"> <Link to={'/donezo'} >Donezo</Link></span>
         </div>
 
-        {/* Search */}
+
         <div className="flex items-center bg-white px-3 py-1 rounded-lg shadow-inner w-1/3">
           <img className="w-5 h-5 rounded" src="https://img.icons8.com/ios/50/search.png" alt="search" />
           <input type="text" placeholder="Search task" className="ml-2 w-full outline-none text-sm" />
           <div className="text-xs text-gray-400 px-2 py-0.5 bg-gray-100 rounded">F</div>
         </div>
 
-        {/* Icons + Avatar */}
+
         <div className="flex items-center space-x-4">
           <img className="w-9 h-9 rounded-full bg-gray-300 p-2" src="https://img.icons8.com/ios/50/new-post.png" alt="mail" />
           <img className="w-9 h-9 rounded-full bg-gray-300" src="https://img.icons8.com/carbon-copy/100/bell--v1.png" alt="bell" />
